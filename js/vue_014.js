9acf39c2d868d7dab0be8f3ee66dd9ea2cf8d196
Haldir65 <mjw090608@gmail.com>
2017-09-04 08:56:55 +0800

    pool

---
+++ js/vue_014.js
@@ -0,0 +1,17 @@
+new Vue({
+  el:'#vue-app',
+  data:{
+    fruits:['apple','watemelon','banana','orange'],
+    recepies:[
+      {name:'A',age:10},
+      {name:'B',age:20},
+      {name:'C',age:30}
+    ]
+  },
+  methods:{
+
+  },
+  computed:{
+
+  }
+});

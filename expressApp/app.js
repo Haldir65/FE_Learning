75bd9c3f4acf7d3b6ab073a76d198fb37c28b9a4
Harris <mjw090608@gmail.com>
2017-12-12 18:57:21 +0800

    new Methods

--- expressApp/app.js
+++ expressApp/app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -70,11 +71,96 @@ var users = [
 
 ]
 
+
+var sample = {
+    code : 0,
+    time : 1469981217,
+    data : {
+      items : [{
+        date : "2016-08-02",
+        state : 'sunny'
+      },
+      {
+        date : "2016-08-03",
+        state : 'Cloudy'
+      }
+      ],
+    }
+  }
+
 app.get('/',function(req,res) {
     res.render('index',{
         title: 'Customers',
         users: users
     });
+});
+
+
+app.get('/users',function (req,res) {
+    var usr = {
+        name: 'John',
+        id : 1
+    }
+
+    var users = [
+        {
+            name: 'John',
+            id : 1
+        },
+        {
+            name: 'Doe',
+            id : 2
+        },
+        {
+            name: 'Dimon',
+            id : 3
+        },
+        {
+            name: 'Sandy',
+            id : 4
+        }
+    ]
+
+ 
+      console.log(req.fresh+' '+req.stale);
+
+    res.set({
+        'Content-Type': 'application/json',
+        'Content-Length': '123',
+        'ETag': '12345',
+        'Cache-Control': 'max-age=5'
+    });
+    res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true });
+    console.log('response send');
+    res.json(sample);
+})
+
+
+app.post('/api/personal', function (req, res) { //针对/api/personal 的 post请求返回
+    // console.log(`request params = ${req.params}`);
+    console.log(JSON.stringify(req.params))
+
+    var output = '';
+    for (var property in req.params) {
+        console.log(property);
+      output += property + ': ' + req.params[property]+'; ';
+    }
+    console.log(output);
+    // console.log(req.body);
+    // console.log(req.body);
+    // console.log(` request body = ${req.body}`);
+    // console.log(`request query = ${req.query}`);
+    // console.log(JSON.stringify(req.query))
+    console.log(req.params)
+	res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+	res.end(JSON.stringify({
+    code : 0,
+    time : 1469981217,
+    data:{
+        name: 'Stuff',
+        honor: sample
+    }
+  }));
 });
 
 app.listen(port,function () {

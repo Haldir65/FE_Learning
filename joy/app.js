4df2ea1268ff6927d65619bdb0718d96929d44c2
Haldir65 <mjw090608@gmail.com>
2018-01-08 22:54:22 +0800

    webpack module bundler

---
+++ joy/app.js
@@ -0,0 +1,16 @@
+var help = require('./help-function');
+
+// help.sayHello("ss");
+
+console.log(help.res);
+
+var data = require('./more')
+console.log(data);
+
+require("./style.css");
+
+const jam = "js"
+
+console.log(jam);
+
+
---
+++ joy/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './app.js',
+    output: {
+        filename: "./bundle.js"
+    },
+    watch: true,
+    module:{
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules$/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}

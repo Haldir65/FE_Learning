29e33d3d4c28257bd7f27399db3584eb29668765
Harris <mjw090608@gmail.com>
2018-01-10 14:21:55 +0800

    switch to branch lily

--- toy/library/first.js
+++
@@ -1,3 +0,0 @@
-export default function greet(params) {
-    console.log('hello');
-}
--- toy/supplier.js
+++
@@ -1,7 +0,0 @@
-const data = {name: 'Harris'};
- const call = () => {
-    console.log(data.name);
-};
-
-call();
-export {data};

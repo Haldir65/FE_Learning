51cdea94a7e5c5a9caa0b73c0a0941378c6a82b7
Harris <mjw090608@gmail.com>
2018-01-29 18:45:24 +0800

    using modules for including handlebars

--- joy/render.js
+++ joy/render.js
@@ -1,18 +1,23 @@
 const bookListingTemplate = require("./template/muscle.hbs");
 
 let addContent = document.addEventListener("DOMContentLoaded", function() {
-	var div = document.createElement('div');
-	div.innerHTML = bookListingTemplate({
-		username: "test",
-		info: "Your books are due next Tuesday",
-		books: [
-			{ title: "A book", synopsis: "With a description" },
-			{ title: "Another book", synopsis: "From a very good author" },
-			{ title: "Book without synopsis" }
-		]
-	});
-	document.body.appendChild(div);
+  var div = document.createElement("div");
+  div.innerHTML = bookListingTemplate({
+    username: "test",
+    info: "Your books are due next Tuesday",
+    books: [
+      { title: "A book", synopsis: "With a description" },
+      { title: "Another book", synopsis: "From a very good author" },
+      { title: "Book without synopsis" }
+    ],
+    people: [
+      { firstName: "Yehuda", lastName: "Katz" },
+      { firstName: "Carl", lastName: "Lerche" },
+      { firstName: "Alan", lastName: "Johnson" }
+    ]
+  });
+  document.body.appendChild(div);
+  console.log(div);
 });
 
-export { addContent }
-
+export { addContent };
--- joy/webpack.config.js
+++ joy/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     filename: "bundle.js"
   },
 
+  resolve: {
+    fallback: path.join(__dirname, "template/helpers")
+  },
   watch: true,
   module: {
     rules: [

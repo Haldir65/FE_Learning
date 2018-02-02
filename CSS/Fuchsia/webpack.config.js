70070b769e430bd6696e66c1780e3b3b1a3edfdb
Harris <mjw090608@gmail.com>
2018-02-02 17:08:28 +0800

    webpack contentBase

---
+++ CSS/Fuchsia/webpack.config.js
@@ -0,0 +1,49 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/ /*end with .css*/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader" // compiles Less to CSS
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    port: 8080,
+    hot: true,
+    inline: true
+  }
+};

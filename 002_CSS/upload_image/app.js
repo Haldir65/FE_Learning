fab859d9d9bde9eb58fcbfaeab3f5230e92bdaf9
Harris <mjw090608@gmail.com>
2018-02-11 18:08:22 +0800

    env

--- 002_CSS/upload_image/app.js
+++ 002_CSS/upload_image/app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var formidable = require("formidable");
 var fs = require("fs");
 var crypto = require("crypto");
+require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "public")));
 

ce2ea74fa47d64c7bd692343ad15977cd08625ea
Harris <mjw090608@gmail.com>
2018-02-11 15:35:21 +0800

    405 method not allowed pending

---
+++ 002_CSS/upload_image/js/app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var formidable = require("formidable");
+var fs = require("fs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.post("/upload", (req, res) => {
+  let form = new formidable.IncomingForm();
+  console.log("==============req incoming ===============");
+  form.multiples = true;
+
+  form.uploadDir = path.join(__dirname, "/uploads");
+
+  form.on("file", (filed, file) => {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  form.on("error", error => {
+    console.log("An error occured" + error);
+  });
+
+  form.on("end", () => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send("success");
+  });
+
+  form.parse(req);
+});
+
+const server = app.listen(5500, () => {
+  console.log("server listen on port 3000");
+});

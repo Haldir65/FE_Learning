26d98b35ea6564578c7588466d9cf1ba313af6d5
Haldir65 <mjw090608@gmail.com>
2018-05-25 22:47:07 +0800

    chat from official example

--- 011_backend/config.js
+++ 011_backend/config.js
@@ -1,10 +1,7 @@
 const port = 9679;
 const domian = "http://localhost";
 
-Authorization =  {
-    api: 'http://localhost:8889/user/',
-    headerName: 'Authorization'
-  };
+
 
 module.exports =
  {port:port,
--- 011_backend/index.js
+++ 011_backend/index.js
@@ -21,6 +21,9 @@ app.use('/birds',birds);
 var community = require('./users/community');
 app.use('/community',community);
 
+var uploadHandler = require('./users/upload');
+app.use('/upload'.uploadHandler);
+
 app.get('/',function(req,res){
     res.send({name:"John",lastName:"Smith",age:32});
 });

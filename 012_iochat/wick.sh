42c4430f53a322237895557b13cdb05d44398f97
Haldir65 <mjw090608@gmail.com>
2018-05-28 08:39:21 +0800

    chat

---
+++ 012_iochat/wick.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#  echo Hello there!
+
+NAME="John wick"
+echo "$NAME"
+echo "your name is ${NAME}"
+
+# uper input 
+read -p "Enter your name " NAME2
+echo "Hello $NAME2, greet you!"
